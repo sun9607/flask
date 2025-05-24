cee71d0c1ef9cca232193e83268a253989540082
sun9607 <qkdcnrclsrn@naver.com>
2025-05-24 19:30:08 +0900

    Update appsettings.py

--- util/appsettings.py
+++ util/appsettings.py
@@ -8,5 +8,5 @@ CORS(app)
 
 UPLOAD_FOLDER = "../uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{os.getenv('SQL_USER')}:{os.getenv('SQL_PASSWORD')}@heliumgas-db.czac4ea085m2.ap-northeast-2.rds.amazonaws.com/invitationcard_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{os.getenv('SQL_USER')}:{os.getenv('SQL_PASSWORD')}@localhost/invitationcard_db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
